e0a15f06d7901dc58f0d6bf770529196f6dac2b9
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-03-23 14:16:49 +0800

    feat: added update api

--- src/server.js
+++ src/server.js
@@ -68,6 +68,54 @@ app.post("/insert", async (req, res) => {
   res.status(200).send(borrowed.toString())
 })
 
+app.post("/update", async (req, res) => {
+  console.log(req.query, typeof req.query.blockNum)
+  const blockNum = parseInt(req.query.blockNum)
+  const address = req.query.address
+  console.log(blockNum, typeof blockNum)
+  let borrowed = 0
+  let temp = null
+  //19445162
+  //19445193
+  try {
+    for (let i = blockNum; i <= 19474082; i++) {
+      console.log("running block", i)
+      try {
+        await Exec(i).then((ret) => {
+          temp = ret
+        })
+      } catch (error) {
+        console.log(error)
+        continue
+      }
+      console.log("HIHIHIIIHi")
+      let data = temp.substring(24)
+      let reserve = data.toString().substring(0, 40)
+      let data2 = data.substring(40)
+      let user = data2.toString().substring(0, 40)
+      if (user != address) {
+        continue
+      }
+      let amount = data2.substring(64)
+      let parse_data = BigInt("0x" + amount)
+      console.log(parse_data)
+      let final_value = await getTokenPrice(reserve, parse_data)
+
+      borrowed = borrowed + final_value
+      console.log("the log:", borrowed)
+    }
+    console.log("Process completed successfully")
+    if (borrowed > 0) {
+      console.log("Borrowed:", borrowed)
+      await updateTableTransaction(address, borrowed)
+    }
+  } catch (error) {
+    console.log("the error:", error)
+  }
+
+  res.status(200).send(borrowed.toString())
+})
+
 app.listen(port, async () => {
   console.log(__dirname + "cle.yaml")
   console.log(`Server is running on http://localhost:${port}`)
